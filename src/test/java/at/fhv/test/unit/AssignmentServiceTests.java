558dc2c1f70d970b20227ab0dd06e47dee5001a8
Felix Lahnsteiner <felix.lahnsteiner@students.fhv.at>
2023-06-13 15:16:22 +0200

    fix tests

--- src/test/java/at/fhv/test/unit/AssignmentServiceTests.java
+++ src/test/java/at/fhv/test/unit/AssignmentServiceTests.java
@@ -2,6 +2,7 @@ package at.fhv.test.unit;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -11,11 +12,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 
 import at.fhv.domain.models.Assignment;
 import at.fhv.domain.persistence.IAssignmentRepository;
 import at.fhv.service.IAssignmentService;
 
+@DirtiesContext
 @SpringBootTest(
     properties = {
         "camunda.bpm.generate-unique-process-engine-name=true",
@@ -37,7 +40,7 @@ public class AssignmentServiceTests {
         LocalDate dueDate = LocalDate.of(2022, 7, 23);
         Assignment assignment = new Assignment("Testassignment", Date.valueOf(dueDate));
 
-        Mockito.when(_assignmentRepository.get(assignment.getAssignemtName())).thenReturn(assignment);
+        Mockito.when(_assignmentRepository.get(anyString())).thenReturn(assignment);
 
         //when .. then
         assertTrue(_assignmentService.isExpired(assignment.getAssignemtName()));
